810e9f23587db63aedf6a4ed2ce26df608924b52
leeejjju <dlwlswn02027@gmail.com>
2024-06-10 01:25:41 +0900

    add seeds, modify action.py to send main_driver.cpp and seeds on REGISTER keyword

--- .github/action.py
+++ .github/action.py
@@ -1,7 +1,7 @@
 import socket
 import struct
 import sys
-
+import os
 
 def send_commit_msg(server_ip, server_port, commit_msg, git_repo):
     # 소켓 생성
@@ -10,24 +10,20 @@ def send_commit_msg(server_ip, server_port, commit_msg, git_repo):
     try:
         # 서버에 접속
         client_socket.connect((server_ip, server_port))
+		# 레포길이 전송
+        git_repo_len = struct.pack("i", len(git_repo)) 
+        client_socket.sendall(git_repo_len)
+        # 레포 전송
+        client_socket.sendall(git_repo.encode())
 
         # MESSAGE에 따라 다른 동작 수행
         if commit_msg.startswith("SUBMIT:"):
             # 숫자 추출
             number_str = commit_msg.split(":")[1]
             number = int(number_str)
-
             # 숫자를 4바이트로 패킹하여 전송
             packed_number = struct.pack("i", number)
             client_socket.sendall(packed_number)
-
-			# 레포길이 전송
-            git_repo_len = struct.pack("i", len(git_repo)) 
-            client_socket.sendall(git_repo_len)
-			# 레포 전송
-            client_socket.sendall(git_repo.encode())
-
-
             # 파일 이름과 파일 내용 전송
             file_name = "submission.cpp"
             send_file(client_socket, file_name)
@@ -36,32 +32,22 @@ def send_commit_msg(server_ip, server_port, commit_msg, git_repo):
             # 0을 4바이트로 패킹하여 전송
             packed_number = struct.pack("i", 0)
             client_socket.sendall(packed_number)
-
-			# 레포길이 전송
-            git_repo_len = struct.pack("i", len(git_repo)) 
-            client_socket.sendall(git_repo_len)
-			# 레포 전송
-            client_socket.sendall(git_repo.encode())
-
-
             # 파일 이름과 파일 내용 전송
             file_name = "test_driver.cpp"
             send_file(client_socket, file_name)
+            file_name = "main_driver.cpp"
+            send_file(client_socket, file_name)
             file_name = "solution.cpp"
             send_file(client_socket, file_name)
+			#seed 전송 
+            for root, _, files in os.walk("seeds"):
+                for file in files:
+                    send_file(client_socket, file)
 
         elif commit_msg == "FINISH":
             # -1을 4바이트로 패킹하여 전송
             packed_number = struct.pack("i", -1)
             client_socket.sendall(packed_number)
-			# 레포길이 전송
-            git_repo_len = struct.pack("i", len(git_repo)) 
-            client_socket.sendall(git_repo_len)
-			# 레포 전송
-            client_socket.sendall(git_repo.encode())
-
-
-
         else:
             print("not a valid commit msg. abort.")
 
@@ -72,20 +58,18 @@ def send_commit_msg(server_ip, server_port, commit_msg, git_repo):
 
 
 def send_file(socket, file_name):
-    # 파일 크기 계산
 
+    # 파일 크기 계산
     with open(file_name, "rb") as file:
         file_data = file.read()
 
     # 파일 이름과 파일 크기를 4바이트로 패킹하여 전송
     packed_filename_size = struct.pack("i", len(file_name)) 
     packed_file_size = struct.pack("i", len(file_data))
-
     socket.sendall(packed_filename_size)
     socket.sendall(file_name.encode())
     socket.sendall(packed_file_size)
     socket.sendall(file_data)
-
     print("sended " + file_name)
 
 
@@ -98,7 +82,6 @@ def main():
     server_port = int(sys.argv[2])
     commit_msg = sys.argv[3]
     git_repo = sys.argv[4]
-
     send_commit_msg(server_ip, server_port, commit_msg, git_repo)
 
 
